c5cdebd18c71f2e6bfdc08561352e4cdb84669e3
(KazikovAP) <KazikovAP@example.com>
2020-04-11 14:15:38 +0300

    histogram.h

---
+++ histogram.h
@@ -0,0 +1,13 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+
+
+
+#endif // HISTOGRAM_H_INCLUDED
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void find_minmax(const vector<double> numbers, double& min, double& max)
