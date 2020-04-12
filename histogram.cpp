fab19a8f235a7e527f63331da2e2b4c109de5285
(KazikovAP) <KazikovAP@example.com>
2020-04-12 21:46:56 +0300

    histogram.cpp

--- histogram.cpp
+++ histogram.cpp
@@ -1,6 +1,6 @@
 #include "histogram.h"
 
-void find_minmax(const vector<double> numbers, double& min, double& max)
+void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
     min = numbers[0];
     max = numbers[0];
