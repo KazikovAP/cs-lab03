50f8239ec074890bf100bc2cb09c18da360cf2be
(KazikovAP) <KazikovAP@example.com>
2020-04-12 19:28:40 +0300

    q

--- main.cpp
+++ main.cpp
@@ -128,11 +128,13 @@ int main()
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
+
     const auto numbers = input_numbers(number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
     double min, max;
+
     find_minmax(numbers,min,max);
     const auto bins = make_histogram(numbers, bin_count);
     show_histogram_svg(bins);
