dcc843c65b18e936259486ad612947ac60c8ea34
(KazikovAP) <KazikovAP@example.com>
2020-04-24 11:46:30 +0300

    lab03svg.h

---
+++ lab03svg.h
@@ -0,0 +1,11 @@
+#ifndef LAB03SVG_H_INCLUDED
+#define LAB03SVG_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> colors(size_t bin_count);
+
+#endif // LAB03SVG_H_INCLUDED
