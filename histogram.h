6211a931b1bde95f0b8cd0531ae8e62c6638d9f1
(KazikovAP) <KazikovAP@example.com>
2020-05-08 12:07:35 +0300

    histogram.h

--- histogram.h
+++ histogram.h
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-void find_minmax(const vector<double>& numbers, double& min, double& max);
+struct Input;
+void find_minmax(struct Input Input, double& min, double& max);
 
 
 #endif // HISTOGRAM_H_INCLUDED
