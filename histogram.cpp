851c3a6fe849b280ae7bb95515a53f563142dc9b
(KazikovAP) <KazikovAP@example.com>
2020-05-11 16:41:43 +0300

    histogram.cpp

--- histogram.cpp
+++ histogram.cpp
@@ -1,16 +1,20 @@
 #include "histogram.h"
+#include <iostream>
+#include <vector>
 
-void find_minmax(Input data, double& min, double& max)
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
-    if (data.numbers.size()>0)
+    if (numbers.size()>0)
 
 
     {
-        min = data.numbers[0];
-        max = data.numbers[0];
+        min = numbers[0];
+        max = numbers[0];
 
 
-        for (double number : data.numbers)
+        for (double number : numbers)
         {
             if (number < min)
             {
@@ -24,3 +28,63 @@ void find_minmax(Input data, double& min, double& max)
     }
 }
 
+vector<size_t> make_histogram (struct Input& data)
+{
+    double min;
+    double max;
+    find_minmax(data.numbers, min, max);
+    vector<size_t> bins(data.bin_count);
+    for (double number : data.numbers)
+    {
+        size_t bin = (size_t)((number - min) / (max - min) * data.bin_count);
+        if (bin == data.bin_count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
+void show_histogram_text(const vector<size_t> &bins)
+{
+
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
