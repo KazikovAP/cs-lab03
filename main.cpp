bb7999dbccbe55ab1fa44ec3f1e1761b472b3b2e
(KazikovAP) <KazikovAP@example.com>
2020-05-22 12:23:40 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include "histogram.h"
 #include "lab03svg.h"
+#include <windows.h>
+#include <stdio.h>
+
+
 
 using namespace std;
 
@@ -78,8 +82,26 @@ void show_histogram_text(const vector<size_t> &bins)
     }
 }
 
+DWORD WINAPI GetVersion(void);
+int printf(const char* format, ...);
+
 int main()
 {
+    DWORD dwVersion = 0;
+    dwVersion = GetVersion();
+    DWORD info = GetVersion();
+    printf("Windows 16x-version is %x\n", info );
+    printf("Windows decimal-version is %u\n", info );
+
+
+    return 0;
+
+    /*int printf(const char* format, ...);
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567);
+
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -92,6 +114,6 @@ int main()
 
     find_minmax(numbers, min, max);
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_svg(bins, bin_count);
-    return 0;
+    show_histogram_svg(bins, bin_count);*/
+
 }
