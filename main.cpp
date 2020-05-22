6f783b3134b2a557c3bd59e4f5327ce0e2bac204
(KazikovAP) <KazikovAP@example.com>
2020-05-22 16:46:23 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
